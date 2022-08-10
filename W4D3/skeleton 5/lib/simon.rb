df2c14230144f3ad6985bc22b52512c1131a5d2a
Daniel Li <Dli53@outlook.com>
2022-08-09 20:34:14 -0400

    simon saids

--- W4D3/skeleton 5/lib/simon.rb
+++ W4D3/skeleton 5/lib/simon.rb
@@ -4,19 +4,32 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @seq = []
+    @sequence_length = 1
+    @game_over = false
   end
 
   def play
-
+    until @game_over == true
+    take_turn
+    end
+    if @game_over == true
+      game_over_message
+      reset_game
+    end
   end
 
   def take_turn
-
+    if @game_over == false
+      show_sequence
+      require_sequence
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
@@ -24,7 +37,7 @@ class Simon
   end
 
   def add_random_color
-
+    @seq << COLORS.sample
   end
 
   def round_success_message
@@ -36,6 +49,8 @@ class Simon
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
