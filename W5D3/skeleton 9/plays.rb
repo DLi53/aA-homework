a782aec7436aaa6f7253aae96dd2400d66ff41d6
Daniel Li <Dli53@outlook.com>
2022-08-16 17:44:09 -0400

    hw

---
+++ W5D3/skeleton 9/plays.rb
@@ -0,0 +1,77 @@
+require 'sqlite3'
+require 'singleton'
+
+class PlayDBConnection < SQLite3::Database
+  include Singleton
+
+  def initialize
+    super('plays.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+class Play
+  attr_accessor :id, :title, :year, :playwright_id
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM plays")
+    data.map { |datum| Play.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @year = options['year']
+    @playwright_id = options['playwright_id']
+  end
+
+  def create
+    raise "#{self} already in database" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.title, self.year, self.playwright_id)
+      INSERT INTO
+        plays (title, year, playwright_id)
+      VALUES
+        (?, ?, ?)
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.title, self.year, self.playwright_id, self.id)
+      UPDATE
+        plays
+      SET
+        title = ?, year = ?, playwright_id = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def self.find_by_title(title)
+      
+  end
+
+  def self.find_by_playwright(name)
+
+  end
+end
+
+
+# In addition, create a Playwright class and add the following methods to our ORM.
+
+# Playwright::all
+# Playwright::find_by_name(name)
+# Playwright#new (this is the initialize method)
+# Playwright#create
+# Playwright#update
+# Playwright#get_plays (returns all plays written by playwright)
+# Remember, our PlayDBConnection class accesses the database stored in plays.db, which includes both the plays and playwrights tables.
+
+class Playwright
+
+  def self.all
+
+  end
+end
